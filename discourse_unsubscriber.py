50c192eb83d96a3c4780dde411b89741d36d053f
Simon Lundström <github-commits@soy.se>
2019-08-14 07:40:05 +0200

    Add main script

---
+++ discourse_unsubscriber.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# pylint: disable=
+"""
+license: ISC
+author: Simon Lundström simmel@soy.se
+"""
+
+__version__ = "0.0.0"
+
+__url__ = "https://github.com/simmel/discourse_unsubscriber"
+
+
+def main():
+    "main"
+    print(__name__)
+
+
+if __name__ == "__main__":
+    main()
