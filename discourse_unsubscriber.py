3b9868d7585acc307cabe387dd544f98932ecccb
Simon Lundström <github-commits@soy.se>
2019-08-15 10:47:10 +0200

    Add arguments

--- discourse_unsubscriber.py
+++ discourse_unsubscriber.py
@@ -5,6 +5,8 @@ license: ISC
 author: Simon Lundström simmel@soy.se
 """
 
+import argparse
+
 __version__ = "0.0.0"
 
 __url__ = "https://github.com/simmel/discourse_unsubscriber"
@@ -12,6 +14,16 @@ __url__ = "https://github.com/simmel/discourse_unsubscriber"
 
 def main():
     "main"
+    parser = argparse.ArgumentParser(
+        description="Unsubscribe from Discourse threads easily"
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--client", action="store_true", help="Start as a client e.g. from mutt"
+    )
+    group.add_argument("--server", action="store_true", help="Start as a server")
+    args = parser.parse_args()
+
     print(__name__)
 
 
